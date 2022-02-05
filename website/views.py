db9720d9be0f930daa47ec5ed0b5c98819160419
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2022-02-05 02:22:59 -0500

    fin

--- website/views.py
+++ website/views.py
@@ -277,4 +277,4 @@ def verify():
 if __name__ == "__main__":
     #port = int(os.environ.get("PORT", 5000))
     #app.run(host='0.0.0.0', port=port)
-    app.run(host='localhost',port=8080)
+    #app.run(host='localhost',port=8080)
